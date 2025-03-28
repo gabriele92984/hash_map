5f6055e6a9744ccfb8f0efdaebfddf71616500d0
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2025-03-28 16:58:54 +0100

    Create #set method

--- hash_map.rb
+++ hash_map.rb
@@ -8,5 +8,31 @@ class HashMap
     @size = 0
   end
 
-  
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code % @capacity
+  end
+
+  def set(key, value)
+    index = hash(key)
+    
+    # Use the following snippet whenever you access a bucket through an index. We want to
+    # raise an error if we try to access an out-of-bounds index
+    raise IndexError if index.negative? || index >= @buckets.size
+    bucket = @buckets[index]
+
+    # Check if a key already exists
+    entry = bucket.find { |pair| pair[0] == key }
+    if entry
+      entry[1] == value
+    else
+      bucket << [key, value]
+      @size += 1
+      resize if need_resize?
+    end
+  end
 end
