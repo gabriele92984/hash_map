1b11c13e33dd56edc82298f972730bae1e6995a3
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2025-03-29 08:15:34 +0100

    Edit #initialize constructor

--- hash_map.rb
+++ hash_map.rb
@@ -12,7 +12,7 @@ class HashMap
   attr_reader :capacity, :load_factor
 
   def initialize(initial_capacity = 16, load_factor = 0.75)
-    @buckets = Array.new(initial_capacity) { [] }
+    @buckets = Array.new(initial_capacity)
     @capacity = initial_capacity
     @load_factor = load_factor
     @size = 0
